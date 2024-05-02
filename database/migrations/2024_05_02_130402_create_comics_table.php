dbd93797e8f11adf8f65daa5cae40b7c9f7d8f89
filippogiovannoni <filippogiovannonii@gmail.com>
2024-05-02 15:19:41 +0200

    run comics migration

--- database/migrations/2024_05_02_130402_create_comics_table.php
+++ database/migrations/2024_05_02_130402_create_comics_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 6, 2);
+            $table->string('category', 30);
             $table->timestamps();
         });
     }
